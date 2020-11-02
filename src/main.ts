6c4b302e1ac898507ae9e6b4c74174076b31417c
cesar <c.astorgagonzalez@gmail.com>
2020-11-01 23:16:53 -0300

    docker config and eureka

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,15 @@
 import { Options } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { registerWithEureka } from './eureka-helper';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api');
   app.enableCors();
   //  /api/endpointName
 
 
   await app.listen(AppModule.port);
+  registerWithEureka("auth", parseInt(AppModule.port));
 }
 bootstrap();
